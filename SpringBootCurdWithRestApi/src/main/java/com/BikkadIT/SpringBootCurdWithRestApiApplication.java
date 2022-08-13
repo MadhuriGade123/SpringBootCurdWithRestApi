dc3316ad5edca2f9b2da395ac1bab8132c917263
unknown <madhurigade50691@gmail.com>
2022-08-13 01:29:22 -0700

    Project added to central repository

---
+++ SpringBootCurdWithRestApi/src/main/java/com/BikkadIT/SpringBootCurdWithRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCurdWithRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCurdWithRestApiApplication.class, args);
+	}
+
+}
